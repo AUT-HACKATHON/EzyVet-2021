2da0c2ad91452ad7295c69efc597967a5e368ce6
MrGeet <aldarbatomunkuev@gmail.com>
2021-05-28 22:26:51 +1200

    Update VetElements.js

--- frontend/src/components/styledComponents/VetElements.js
+++ frontend/src/components/styledComponents/VetElements.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 import media from 'css-in-js-media';
 
-export const VetCard = styled.img`
+export const VetCard = styled.div`
 	width: 100%;
 	display: block;
 	object-fit: cover;
+
+	${media('>=tablet')} {
+		border-width: 1px;
+		border-top-left-radius: 2.5rem;
+	}
+
 	${media('<tablet')} {
 		display: none;
+	}
+
+	& > div {
 	}
 `;
