f251f96f20fd2ec257e745982d43cb1fe21b3124
Ashley Thew <ashl.e.thew@gmail.com>
2021-05-28 13:51:12 +1200

    VetContext replace
    Replace fetching in screen with VetContext

--- frontend/src/Screens/ListScreen.js
+++ frontend/src/Screens/ListScreen.js
@@ -1,38 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { getDistance } from 'geolib';
+import React, { useContext } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import VetItemList from '../components/VetItemList';
-import axios from 'axios';
+import { VetContext } from '../context/VetContext';
 
 const ListScreen = ({ history }) => {
-	const [vetData, setVetData] = useState();
+	const { vetData } = useContext(VetContext);
 
-	useEffect(() => {
-		if (!vetData && 'geolocation' in navigator) {
-			const fetchVets = async () => {
-				const { data: vets } = await axios.get('/api/vets/list');
-				console.log('Available');
-				navigator.geolocation.getCurrentPosition(function (position) {
-					const userCoords = {
-						latitude: position.coords.latitude,
-						longitude: position.coords.longitude,
-					};
-					for (const vet of vets) {
-						const coord = { latitude: vet.location.lat, longitude: vet.location.lng };
-						vet.distance = getDistance(userCoords, coord);
-					}
-					vets.sort((a, b) => {
-						// const coordA = { latitude: a.location.lat, longitude: a.location.lng };
-						// const coordB = { latitude: b.location.lat, longitude: b.location.lng };
-						// return getDistance(userCoords, coordB) - getDistance(userCoords, coordA);
-						return a.distance - b.distance;
-					});
-					setVetData(vets);
-				});
-			};
-			fetchVets();
-		}
-	}, [vetData, setVetData]);
 	return (
 		<>
 			<h1>Vet clinics</h1>
