574e6e1ffc7c161e50eaf0e9d34683c001def2f8
Ashley Thew <ashl.e.thew@gmail.com>
2021-05-28 13:19:34 +1200

    Fetch vets list
    Add fetching vet list from backend

--- frontend/src/Screens/ListScreen.js
+++ frontend/src/Screens/ListScreen.js
@@ -1,34 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { vets } from '../data/data';
 import { getDistance } from 'geolib';
+import axios from 'axios';
 
 const ListScreen = ({ history }) => {
 	const [vetData, setVetData] = useState();
 
 	useEffect(() => {
 		if (!vetData && 'geolocation' in navigator) {
-			console.log('Available');
-			navigator.geolocation.getCurrentPosition(function (position) {
-				const userCoords = {
-					latitude: position.coords.latitude,
-					longitude: position.coords.longitude,
-				};
-				vets.sort((a, b) => {
-					const coordA = { latitude: a.location.lat, longitude: a.location.lng };
-					const coordB = { latitude: b.location.lat, longitude: b.location.lng };
-					return getDistance(userCoords, coordB) - getDistance(userCoords, coordA);
+			const fetchVets = async () => {
+				const { data } = await axios.get('/api/vets/list');
+				console.log('Available');
+				navigator.geolocation.getCurrentPosition(function (position) {
+					const userCoords = {
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude,
+					};
+					data.sort((a, b) => {
+						const coordA = { latitude: a.location.lat, longitude: a.location.lng };
+						const coordB = { latitude: b.location.lat, longitude: b.location.lng };
+						return getDistance(userCoords, coordB) - getDistance(userCoords, coordA);
+					});
+					setVetData(data);
 				});
-				setVetData(vets);
-			});
+			};
+			fetchVets();
 		}
 	}, [vetData, setVetData]);
 	return (
 		<>
 			List:
 			<br />
-			{vets.map((value, index) => {
-				return <li key={index}>{JSON.stringify(value)}</li>;
-			})}
+			{vetData &&
+				vetData.map((value, index) => {
+					return <li key={index}>{JSON.stringify(value)}</li>;
+				})}
 		</>
 	);
 };
